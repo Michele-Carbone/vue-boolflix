3b690e7c6c13e5a09bde31b0acb2c39cbf112085
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-09-23 23:12:02 +0800

    added search

---
+++ src/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" :placeholder="placeholder" v-model.trim="inputValue" />
+    <button type="submit" @click="emitSearch">
+      {{ buttonText || "Cerca" }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  props: ["placeholder", "buttonText"],
+  methods: {
+    emitSearch() {
+      this.$emit("search", this.inputValue);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
